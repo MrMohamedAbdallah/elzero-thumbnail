65c05f1480f47ff96bc235cb075bc7bff8b673b7
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-04-09 02:35:11 +0200

    Allow user to write the video number on the image
    [+] Added video's number font, color and position

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from PIL import ImageDraw
 
 file = "elzero.jpg";
 videoName = "Tuples & Methods \nPart 2"
+videoNumber = "25"
+
+# Adding # sign and zeroz
+if len(videoNumber) < 3:
+    videoNumber = "#" + ((3 - len(videoNumber)) * "0") + videoNumber
+
+
 outputFile = "output/elzero_output.jpg"
 
 
@@ -13,13 +20,21 @@ img = Image.open(file)
 
 draw = ImageDraw.Draw(img)  # Draw object
 
-videoNameFont = ImageFont.truetype("fonts/Nunito-Bold.ttf", 75)
+# Fonts
+videoNameFont = ImageFont.truetype("fonts/Nunito-Bold.ttf", 72)
+videoNumberFont = ImageFont.truetype("fonts/gadugib.ttf", 78)
 
 
 # Write on the image
-vidoeNamePosition = (84, 236)
+vidoeNamePosition = (84, 210)
 videoNameColor = (255, 255, 255) # white color
 
+vidoeNumberPosition = (15, 595)
+videoNumberColor = (2, 52, 94) # #02345e
+
 draw.text(vidoeNamePosition, videoName, videoNameColor, font=videoNameFont)
+
+draw.text(vidoeNumberPosition, videoNumber, videoNumberColor, font=videoNumberFont)
+
 
 img.save(outputFile)
